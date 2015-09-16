50aa0c2662a6d684dc7ffed5d0df1941e4a4e4aa
Rodrigo Vieira <rodrigo.conspena@gmail.com>
2015-09-16 20:32:43 -0300

    0.3.1 Alpha
    - Adição de classes de teste.

---
+++ Test/Html/UTestAtributo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetZ.Web.Html;
+
+namespace NetZ.WebTest.Html
+{
+    [TestClass]
+    public class UTestAtributo
+    {
+        [TestMethod]
+        public void getStrFormatadoTest()
+        {
+            Atributo att = new Atributo("atributo_teste", "atributo_valor");
+
+            att.addValor("atributo_valor_1");
+            att.addValor("atributo_valor_2");
+
+            Assert.AreEqual("atributo_teste=\"atributo_valor atributo_valor_1 atributo_valor_2\"", att.getStrFormatado());
+
+            att = new Atributo("atributo_sem_valor");
+
+            Assert.AreEqual("atributo_sem_valor", att.getStrFormatado());
+
+            att = new Atributo("atributo_varios_valores");
+
+            att.strSeparador = ";";
+
+            att.addValor(150.50m);
+            att.addValor(10);
+
+            Assert.AreEqual("atributo_varios_valores=\"150.50;10\"", att.getStrFormatado());
+        }
+    }
+}
---
+++ Test/Html/UTestAtributoCss.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetZ.Web.Html;
+
+namespace NetZ.WebTest.Html
+{
+    [TestClass]
+    public class UTestAtributoCss
+    {
+        [TestMethod]
+        public void getStrFormatadoTest()
+        {
+            AtributoCss attCss = new AtributoCss("c100", "backgroundcolor", "blue");
+
+            Assert.AreEqual(".c100{backgroundcolor:blue}", attCss.getStrFormatado());
+
+            attCss = new AtributoCss("c101", "margin", "10px");
+
+            attCss.addValor("11px");
+            attCss.addValor("12px");
+            attCss.addValor("13px");
+
+            Assert.AreEqual(".c101{margin:10px 11px 12px 13px}", attCss.getStrFormatado());
+        }
+    }
+}
