17f9b523150768de667bc7dc014411a15be8206c
Rodrigo Daros <rodrigo.conspena@gmail.com>
2017-03-15 17:08:53 -0300

    Sentinela:
    - Adição de arquivos principais de design.
    - Adição das classes de controle de activities.

---
+++ Html/Activity/ActivityBase.cs
@@ -0,0 +1,27 @@
+﻿using NetZ.Web.Html.Componente;
+
+namespace NetZ.Web.Html.Activity
+{
+    public abstract class ActivityBase : ComponenteHtml
+    {
+        #region Constantes
+
+        #endregion Constantes
+
+        #region Atributos
+
+        #endregion Atributos
+
+        #region Construtores
+
+        #endregion Construtores
+
+        #region Métodos
+
+        #endregion Métodos
+
+        #region Eventos
+
+        #endregion Eventos
+    }
+}
