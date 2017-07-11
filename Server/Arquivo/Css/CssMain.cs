0dbdfe83676e392e9bee4b49d3455e25933dec49
Aloisio <a-bruske@relatar.com.br>
2017-07-11 20:24:51 -0300

    Site Relatar:
    - Implementado a página empresa;
    - Alterado o layout padrão do scrollbar;
    - Modificações de padronização das páginas de produto, colaboradores e contato;

--- Server/Arquivo/Css/CssMain.cs
+++ Server/Arquivo/Css/CssMain.cs
@@ -48,11 +48,16 @@ namespace NetZ.Web.Server.Arquivo.Css
 
             this.strHref = (STR_CSS_SRC + "?" + DateTime.Now.ToString("yyyyMMddHHmm"));
 
-            this.addCssPuro("::-webkit-scrollbar-corner{background-color:rgb(239,239,239)}");
-            this.addCssPuro("::-webkit-scrollbar-thumb{background-color:rgb(80,80,80);border:1px solid rgb(195,195,195)}");
-            this.addCssPuro("::-webkit-scrollbar-track{background-color:rgb(239,239,239)}");
-            this.addCssPuro("::-webkit-scrollbar{height:8px;width:8px}");
-            this.addCssPuro("a{color:#428bca;text-decoration:none}");
+            this.addCssPuro("::-webkit-scrollbar{margin-right:15px;height:10px;width:10px;background-color:rgba(0,0,0,0.0)}");
+            this.addCssPuro("::-webkit-scrollbar-button{height:5px;width:5px;background-color:rgba(0,0,0,0.0)}");
+            this.addCssPuro("::-webkit-scrollbar-track{background-color:rgba(0,0,0,0.0);border-radius:5px;}");
+            this.addCssPuro("::-webkit-scrollbar-thumb{background-color:rgba(80,80,80,0.5);border-radius:5px;}");
+            this.addCssPuro("::-webkit-scrollbar-corner{border-radius:5px;}");
+            
+            this.addCssPuro("a:link{color:inherit;text-decoration:none;}");
+            this.addCssPuro("a:visited{color:inherit;text-decoration:none;}");
+            this.addCssPuro("a:hover{color:inherit;text-decoration:none;}");
+            this.addCssPuro("a:active{color:inherit;text-decoration:none;}");
         }
 
         #endregion Métodos
